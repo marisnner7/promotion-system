2335d7ad117037845afe9206e91e627b1821c623
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-08 09:42:41 -0300

    created a model for promotions

---
+++ app/models/promotion.rb
@@ -0,0 +1,7 @@
+class Promotion < ApplicationRecord
+
+  validates :name, :code, :discount_rate,
+            :coupon_quantity, :expiration_date, presence: { message: "não pode ficar em branco" }
+  validates :code, uniqueness: { message: "deve ser único" }
+
+end
