42a4c5135c3f7ac0b34cdbd1c8db56d5b5aa9ce5
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-17 16:24:54 -0300

    created routes for api

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,15 @@ Rails.application.routes.draw do
     post 'generate_coupons', on: :member
   end
   resources :product_categories, only: %i[index show new create]
+
+  resources :coupons, only: [] do
+    post 'inactivate', on: :member
+    post 'reactivate', on: :member
+  end
+
+  namespace 'api' do
+    namespace 'v1' do
+      get 'coupons/:code', to: 'coupons#show'
+    end
+  end
 end
