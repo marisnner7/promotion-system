2d6ef95195c0f69196191a4e9d7b95542f6bf9fc
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-21 11:48:32 -0300

    added join table prod_cat and coupon

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,50 +12,60 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_16_041852) do
-
+ActiveRecord::Schema.define(version: 20_210_219_161_239) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "coupons", force: :cascade do |t|
-    t.string "code"
-    t.string "order"
-    t.bigint "promotion_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.integer "status", default: 0
-    t.index ["promotion_id"], name: "index_coupons_on_promotion_id"
+  create_table 'coupons', force: :cascade do |t|
+    t.string 'code'
+    t.string 'order'
+    t.bigint 'promotion_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.integer 'status', default: 0
+    t.index ['promotion_id'], name: 'index_coupons_on_promotion_id'
   end
 
-  create_table "product_categories", force: :cascade do |t|
-    t.string "name"
-    t.string "code"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'product_categories', force: :cascade do |t|
+    t.string 'name'
+    t.string 'code'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  create_table "promotions", force: :cascade do |t|
-    t.string "name"
-    t.string "description"
-    t.string "code"
-    t.decimal "discount_rate"
-    t.integer "coupon_quantity"
-    t.date "expiration_date"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+  create_table 'product_category_promotions', force: :cascade do |t|
+    t.bigint 'product_category_id', null: false
+    t.bigint 'promotion_id', null: false
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['product_category_id'], name: 'index_product_category_promotions_on_product_category_id'
+    t.index ['promotion_id'], name: 'index_product_category_promotions_on_promotion_id'
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  create_table 'promotions', force: :cascade do |t|
+    t.string 'name'
+    t.string 'description'
+    t.string 'code'
+    t.decimal 'discount_rate'
+    t.integer 'coupon_quantity'
+    t.date 'expiration_date'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
   end
 
-  add_foreign_key "coupons", "promotions"
+  create_table 'users', force: :cascade do |t|
+    t.string 'email', default: '', null: false
+    t.string 'encrypted_password', default: '', null: false
+    t.string 'reset_password_token'
+    t.datetime 'reset_password_sent_at'
+    t.datetime 'remember_created_at'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+    t.index ['email'], name: 'index_users_on_email', unique: true
+    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  end
+
+  add_foreign_key 'coupons', 'promotions'
+  add_foreign_key 'product_category_promotions', 'product_categories'
+  add_foreign_key 'product_category_promotions', 'promotions'
 end
