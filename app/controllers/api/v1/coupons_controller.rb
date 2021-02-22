9eeb7050eeb57fa57ada56317d5537205c0c28d6
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-22 15:57:37 -0300

    coupon burn methods

--- app/controllers/api/v1/coupons_controller.rb
+++ app/controllers/api/v1/coupons_controller.rb
@@ -6,29 +6,37 @@ module Api
       before_action :set_coupon, only: %i[show burn]
 
       def show
-        render json: @coupon.promotion, status: :ok
+        if @coupon.promotion.expired?
+          render json: 'Cupom expirado', status: :precondition_failed
+        else
+          render json: @coupon.promotion, status: :ok
+        end
       end
 
       def burn
         @coupon.order = params.require(:order).permit(:code)[:code]
-        @coupon.burn!
+        return render json: 'Categoria inválida para cupom', status: :bad_request unless @coupon.valid_category? params[:category]
+        @coupon.burn!(params.require(:order).permit(:code)[:code])
+        
+        @coupon.update(order: params[:order][:code], status: :burn)
+        
         render json: 'Cupom utilizado com sucesso', status: :ok
-      rescue ActionController::ParameterMissing
-        render json: '', status: :precondition_failed
+        
+        rescue ActionController::ParameterMissing
+          render json: '', status: :precondition_failed
+        rescue ActiveRecord::RecordInvalid
+          render json: '', status: 422
       end
-
+      
+      
       private
-
-      def verify_order_code
-        return if params.dig(:order, :code)
-
-        render json: '', status: :precondition_failed
-      end
-
+      
+     
+      
       def set_coupon
         @coupon = Coupon.find_by!(code: params[:code])
-      rescue ActiveRecord::RecordNotFound
-        render json: 'Cupom não encontrado', status: 404
+        rescue ActiveRecord::RecordNotFound
+          render json: 'Cupom não encontrado', status: 404
       end
     end
   end
