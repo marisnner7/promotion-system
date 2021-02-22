e4541a4c63d6c147ffac1f6f87bae0666748c5d3
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-22 15:58:11 -0300

    new seeds with category realated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -10,12 +12,31 @@ require 'faker'
 
 puts 'Cleaning DB'
 
-ProductCategory.delete_all
+Promotion.destroy_all
+ProductCategoryPromotion.destroy_all
+ProductCategory.destroy_all
 puts 'Products Categories deleted'
 puts 'Deleting promotions'
-Promotion.delete_all
+
+puts 'Creating products categories'
+
+
+product_categories = []
+pc1 = ProductCategory.create!(name: 'Hospedagem', code: 'HOSP')
+pc2 = ProductCategory.create!(name: 'Email', code: 'EMAIL')
+pc3 = ProductCategory.create!(name: 'Cloud', code: 'CLOU')
+
+
+product_categories << pc1
+product_categories << pc2
+product_categories << pc3
+
+
+puts 'products category created'
+
 
 puts 'Creating Promotions'
+
 8.times do
   promotion = Promotion.create!(
     name: Faker::Company.name,
@@ -23,19 +44,11 @@ puts 'Creating Promotions'
     code: Faker::Barcode.ean(8),
     discount_rate: rand(1..15).truncate(2),
     coupon_quantity: rand(1..20),
-    expiration_date: Faker::Date.forward(days: 45)
+    expiration_date: Faker::Date.forward(days: 45),
+    product_category_ids: (product_categories.shuffle.first).id
   )
 end
+
+
 puts 'Promotions Created!'
-
-puts 'Creating products categories'
-2.times do
-  promotion = ProductCategory.create!(
-    name: Faker::Company.name,
-    code: Faker::Barcode.ean(8),
-  )
-end
-
-puts 'products category created'
-
 
