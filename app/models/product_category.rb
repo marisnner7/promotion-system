f81b13e5fc85101f5259a33e8b1747323e92c848
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-22 15:30:15 -0300

    validations, associations fix

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class ProductCategory < ApplicationRecord
-  has_many :product_category_promotions
+  has_many :product_category_promotions, dependent: :delete_all
   has_many :promotions, through: :product_category_promotions
 
   validates :name, :code, presence: { message: 'não pode ficar em branco' }
-  validates :code, uniqueness: { message: 'deve ser único' }
+  validates :name, :code, uniqueness: { message: 'já está em uso' }
 end
