bd93e540beb8a77a1bb3676072efaf629f65a549
Marianne Aguiar <mariannehaguiar@gmail.com>
2021-02-22 18:21:22 -0300

    dependent destroy to product category from promot

--- app/models/promotion.rb
+++ app/models/promotion.rb
@@ -4,7 +4,7 @@ class Promotion < ApplicationRecord
   has_many :coupons, dependent: :destroy
   belongs_to :user, class_name: 'User', foreign_key: 'creator_id', optional: true
   belongs_to :user, class_name: 'User', foreign_key: 'approver_id', optional: true
-  has_many :product_category_promotions
+  has_many :product_category_promotions, dependent: :destroy
   has_many :product_categories, through: :product_category_promotions
 
   validates :name, :code, :discount_rate,
